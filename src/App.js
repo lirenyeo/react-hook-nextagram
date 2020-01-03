eb328d7b2a9e51dbe5ea035943ec02f728b72232
Liren Yeo <liren90@gmail.com>
2020-01-03 11:17:59 +0800

    liren asked me to add a newline to demonstrate git

--- src/App.js
+++ src/App.js
@@ -13,6 +13,7 @@ import UploadPage from './pages/UploadPage'
 
 const AuthContext = createContext(null)
 
+
 const App = () => {
   const [currentUser, setCurrentUser] = useState({
     user: localStorage.user ? JSON.parse(localStorage.user) : null,
