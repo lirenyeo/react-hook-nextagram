ef8d0d2059099c4fc84ea309154dbae56b43a2ee
Liren Yeo <liren90@gmail.com>
2020-02-07 17:20:43 +0800

    Page transition using Fading, and animted h3 in profile page

--- src/components/AnimatedSwitch.js
+++ src/components/AnimatedSwitch.js
@@ -2,68 +2,25 @@ import React from "react";
 import { Switch } from "react-router-dom";
 import posed, { PoseGroup } from "react-pose";
 
-/**
- * This component is used to control the routing animation.
- * It controls what should happen after animation complete (onRest).
- * It differs animation direction based on routePopped props. (Set in router.action.js and available from routerReducer).
- * @param location React router location used as key in Switch
- * @param children All routes (set in Routes.js)
- * @param routePopped Used to manage direction of animation
- * @param rest All other props sent down
- */
-export const AnimatedSwitch = ({ history, location, children, ...rest }) => {
-  const reverse = location.pathname === "/";
+const RoutesContainer = posed.div({
+  enter: {
+    opacity: 1,
+    delay: 300,
+    beforeChildren: true
+  },
+  exit: { opacity: 0 }
+});
 
+export const AnimatedSwitch = ({ history, location, children, ...rest }) => {
   return (
-    <PoseGroup
-      flipMove={false}
-      preEnterPose={reverse ? "leftSide" : "rightSide"}
-      exitPose={reverse ? "rightSide" : "leftSide"}
-    >
-      <ContextRouteAnimation key={location.pathname} reverse={reverse}>
+    <PoseGroup>
+      <RoutesContainer key={location.key} >
         <Switch location={location} {...rest}>
           {children}
         </Switch>
-      </ContextRouteAnimation>
+      </RoutesContainer>
     </PoseGroup>
   );
 };
 
 export default AnimatedSwitch;
-
-
-/**
- * Try to change up the different commented values for varying animatmions
- */
-export const ContextRouteAnimation = posed.div({
-  enter: {
-    x: 0,
-    // opacity: 1,
-    // scale: 1,
-    transition: {
-      type: "tween",
-      ease: "easeInOut",
-      duration: 400
-    }
-  },
-  leftSide: {
-    x: "-100%",
-    // opacity: 0,
-    // scale: 1.5,
-    transition: {
-      type: "tween",
-      ease: "easeInOut",
-      duration: 400
-    }
-  },
-  rightSide: {
-    x: "100%",
-    // opacity: 0,
-    // scale: 1.5,
-    transition: {
-      type: "tween",
-      ease: "easeInOut",
-      duration: 400
-    }
-  }
-});
--- src/pages/MyProfilePage.js
+++ src/pages/MyProfilePage.js
@@ -2,6 +2,17 @@ import React, { useEffect, useState } from 'react'
 import { Container } from 'reactstrap'
 import TowPic from '../images/tow.svg'
 import Axios from 'axios'
+import posed from 'react-pose'
+
+const PosedContainer = posed.div({
+  enter: { staggerChildren: 50 },
+  exit: { staggerChildren: 20, staggerDirection: -1 }
+});
+
+const PosedH3 = posed.h3({
+  enter: { y: 0, opacity: 1 },
+  exit: { y: 50, opacity: 0 }
+});
 
 const MyProfilePage = ({ currentUser }) => {
   const [myImages, setMyImages] = useState([])
@@ -16,36 +27,38 @@ const MyProfilePage = ({ currentUser }) => {
   }, [])
 
   return (
-    <Container
-      style={{
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        flexDirection: 'column',
-        paddingTop: '50px'
-      }}
-    >
-      <img style={{ maxWidth: '400px', width: '100%' }} src={TowPic} alt="under construction" />
-      <h3 className="text-muted text-center mt-5">
-        Hey {currentUser.user.username}! Sorry, your page is under construction!
-      </h3>
-      {myImages.length ? (
-        <div>
-          <p className="text-center mt-5">
-            But I think these are your pictures:
-          </p>
-          {myImages.map((url, idx) => (
-            <img
-              key={idx}
-              src={url}
-              className="m-2"
-              height="50"
-              alt="user uploaded pic"
-            />
-          ))}
-        </div>
-      ) : null}
-    </Container>
+    <PosedContainer>
+      <Container
+        style={{
+          display: 'flex',
+          justifyContent: 'center',
+          alignItems: 'center',
+          flexDirection: 'column',
+          paddingTop: '50px'
+        }}
+      >
+        <img style={{ maxWidth: '400px', width: '100%' }} src={TowPic} alt="under construction" />
+        <PosedH3 className="text-muted text-center mt-5">
+          Hey {currentUser.user.username}! Sorry, your page is under construction!
+        </PosedH3>
+        {myImages.length ? (
+          <div>
+            <p className="text-center mt-5">
+              But I think these are your pictures:
+            </p>
+            {myImages.map((url, idx) => (
+              <img
+                key={idx}
+                src={url}
+                className="m-2"
+                height="50"
+                alt="user uploaded pic"
+              />
+            ))}
+          </div>
+        ) : null}
+      </Container>
+    </PosedContainer>
   )
 }
 
